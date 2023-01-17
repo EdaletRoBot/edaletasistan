0f2fd73446b122d46eecb0672afabd28153c114d
Fakebody31 <99437747+Fakebody31@users.noreply.github.com>
2023-01-17 18:37:49 +0400

    Update and rename broadcast.py to edalet.py

---
+++ edalet.py
@@ -0,0 +1,7 @@
+# Bu repo edalet_22 tərəfindən 18.01.2023 tarixində yığılıb
+# Bu repodan icazəsiz hər hansı kodu sətri məlumatı kopyalıyıb
+# Öz adına çıxaran peysərdi
+# Bu yazıları silmədən işlədin
+
+# t.me/RoBotlarimTg | t.me/edaletsup
+# t.me/edalet_22
