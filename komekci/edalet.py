ec4a619c336caf7256d8e5b0407d4a1aa9ae0aba
Fakebody31 <99437747+Fakebody31@users.noreply.github.com>
2023-01-18 17:30:50 +0400

    Update edalet.py

--- komekci/edalet.py
+++ komekci/edalet.py
@@ -9,4 +9,4 @@ API_HASH = "64a0620c2644ceff0c1058a4ffc861ad"
 bot_token = "5883816340:AAFJpGNHNPNvogeKaLH0duNJjpchsmQ1UOg"
 
 # Edalet
-Edalet = TelegramClient('Edalet', api_id, api_hash).start(bot_token=bot_token)
+Edalet = TelegramClient('Edalet', API_ID, API_HASH).start(bot_token=bot_token)
