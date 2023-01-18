d6e96bbf528789f2d80d6f27607590b37dc68040
Fakebody31 <99437747+Fakebody31@users.noreply.github.com>
2023-01-18 17:17:06 +0400

    Update edalet.py

--- edalet.py
+++ edalet.py
@@ -507,9 +507,11 @@ tekli_calisan = []
   		
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
-@client.on(events.NewMessage(pattern='(?i)/start+'))
-async def yeni_mesaj(event: events.NewMessage.Event):
-    await event.reply(f"👋🏻 Salam mən @edalet_22 nin asistaniyam\nMənə start verdiyin hakkında məlumatı Sahibimə dedim 📨")
+@client.on(events.NewMessage(pattern='(?i)/start'))
+async def handler(event):
+   await event.reply('f"👋🏻 Salam mən @edalet_22 nin asistaniyam\nMənə start verdiyin hakkında məlumatı Sahibimə dedim 📨"')
+
+
  
 
 
