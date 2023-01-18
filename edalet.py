f2410f45db288ca65c68ec225d425afb103dd1a7
Fakebody31 <99437747+Fakebody31@users.noreply.github.com>
2023-01-18 16:15:45 +0400

    Update edalet.py

--- edalet.py
+++ edalet.py
@@ -522,6 +522,9 @@ async def delcmdc(bot: Client, message: Message):
 Edalet = TelegramClient('Edalet', API_ID, API_HASH).start(bot_token=bot_token)
 
 
+API_ID = 28054551
+API_HASH = "64a0620c2644ceff0c1058a4ffc861ad"
+bot_token = "5883816340:AAFJpGNHNPNvogeKaLH0duNJjpchsmQ1UOg"
   
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------#
   
