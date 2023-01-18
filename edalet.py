5d92967669750aeebdf32a58036f30c6ece4bdca
Fakebody31 <99437747+Fakebody31@users.noreply.github.com>
2023-01-18 16:09:23 +0400

    Update edalet.py

--- edalet.py
+++ edalet.py
@@ -38,13 +38,10 @@ LOGGER = logging.getLogger(__name__)
 api_id = Config.API_ID
 api_hash = Config.API_HASH
 bot_token = Config.BOT_TOKEN
-bot_username = Config.BOT_USERNAME
-support = Config.SUPPORT_CHAT
-owner = Config.OWNER_USERNAME
-bot_name = Config.BOT_NAME
 
 
-SUDO_USERS = Config.SUDO_USERS
+
+
 
 #-#-#-# Pyrogram Başlanğıc #-#-#-#
 app = Client(":memory:", api_id, api_hash, bot_token=bot_token)
