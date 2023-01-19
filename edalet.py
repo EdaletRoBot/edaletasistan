b563277935aa03156c27848ea205d1d7ceae36dd
Fakebody31 <99437747+Fakebody31@users.noreply.github.com>
2023-01-19 18:06:19 +0400

    Update edalet.py

--- edalet.py
+++ edalet.py
@@ -514,6 +514,52 @@ def command1(bot, message):
 
 
 
+async def make_carbon(code):
+    url = "https://carbonara.vercel.app/api/cook"
+    async with aiosession.post(url, json={"code": code}) as resp:
+        image = BytesIO(await resp.read())
+    image.name = "carbon.png"
+    return image
+
+
+@app.on_message(filters.command('carbon') & filters.me)
+async def carbon_func(bot: app, message: Message):
+    text = (
+        message.text.split(None, 1)[1]
+        if len(
+            message.command,
+        )
+        != 1
+        else None
+    )
+    if message.reply_to_message:
+        text = message.reply_to_message.text or message.reply_to_message.caption
+    if not text:
+        return await message.delete()
+    ex = await message.edit_text("`Preparing Carbon . . .`")
+    carbon = await make_carbon(text)
+    await ex.edit("`Uploading . . .`")
+    await asyncio.gather(
+        ex.delete(),
+        client.send_photo(
+            message.chat.id,
+            carbon,
+            caption=f"**Carbonised by** {client.me.mention}",
+            reply_to_message_id=ReplyCheck(message),
+        ),
+    )
+    carbon.close()
+
+
+add_command_help(
+    "carbon",
+    [
+        ["carbon <reply>", "Carbonize text with default settings."],
+    ],
+)
+
+
+
 	
 print(">> Bot işləyir narahat olmayın. @edalet_22 Məlumat almaq üçün <<")
 app.run()
