de5c5bcd7bc21496a626a0ea965c09c757fd3a9f
Fakebody31 <99437747+Fakebody31@users.noreply.github.com>
2023-01-20 22:03:14 +0400

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,18 +1,10 @@
+#İçazəsiz ğötürsən at yaşa 😘😘
+
 import os
 
-
-class Config():
-    # Bu dəyərləri my.telegram.org saytından əldə edin
-    #>>> https://my.telegram.org
-    API_ID = int(os.environ.get("API_ID","28054551"))
-    API_HASH = os.environ.get("API_HASH","64a0620c2644ceff0c1058a4ffc861ad")
-    BOT_TOKEN = "5883816340:AAFJpGNHNPNvogeKaLH0duNJjpchsmQ1UOg"
-    BOT_USERNAME = os.environ.get("BOT_USERNAME","dejavy")
-    BOT_NAME = os.environ.get("BOT_NAME","EdaletRoBot")
-    BOT_ID = int(os.environ.get("BOT_ID","5883816340"))
-    SUDO_USERS = os.environ.get("SUDO_USERS","edalet_22").split()
-    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT","5540993505")
-    OWNER_ID = int(os.environ.get("OWNER_ID","5540993505"))
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME","edalet_22")
+class Config(object):
+    APP_ID = int(os.environ.get("APP_ID", 6))
+    API_HASH = os.environ.get("API_HASH", None)
+    TOKEN = os.environ.get("TOKEN", None)
     
- 
+ #---------------------------------------------------------------------------@edalet_22-----------------------------------------------------------------------------------#
