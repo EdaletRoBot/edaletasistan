173545d6c852dad5baece12bb353474e9705fa30
Fakebody31 <99437747+Fakebody31@users.noreply.github.com>
2023-01-20 21:55:56 +0400

    Create untils.py

---
+++ Plugins/untils.py
@@ -0,0 +1,14 @@
+import sys
+import logging
+import importlib
+from pathlib import Path
+
+def load_mahoaga(plugin_name):
+    path = Path(f"Plugins/edalet/{plugin_name}.py")
+    name = "Plugins.edalet.{}".format(plugin_name)
+    spec = importlib.util.spec_from_file_location(name, path)
+    load = importlib.util.module_from_spec(spec)
+    load.logger = logging.getLogger(plugin_name)
+    spec.loader.exec_module(load)
+    sys.modules["Plugins.edalet." + plugin_name] = load
+    print("Bot Başladı " + plugin_name)
