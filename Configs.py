f7e81812f6c8a674088bba06aded8c7c526a9e14
Fakebody31 <99437747+Fakebody31@users.noreply.github.com>
2023-01-20 23:03:43 +0400

    Update and rename Config.py to Configs.py

--- Configs.py
+++ Configs.py
@@ -3,8 +3,8 @@
 import os
 
 class Config(object):
-    APP_ID = int(os.environ.get("APP_ID", 6))
-    API_HASH = os.environ.get("API_HASH", None)
-    TOKEN = os.environ.get("TOKEN", None)
+    APP_ID = int(os.environ.get("28054551", 6))
+    API_HASH = os.environ.get("64a0620c2644ceff0c1058a4ffc861ad", None)
+    TOKEN = os.environ.get("5883816340:AAFJpGNHNPNvogeKaLH0duNJjpchsmQ1UOg", None)
     
  #---------------------------------------------------------------------------@edalet_22-----------------------------------------------------------------------------------#
