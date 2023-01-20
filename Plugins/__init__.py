0e7b9e1a670e35c7635fe5f219dcae6985503eab
Fakebody31 <99437747+Fakebody31@users.noreply.github.com>
2023-01-20 23:20:28 +0400

    Update __init__.py

--- Plugins/__init__.py
+++ Plugins/__init__.py
@@ -5,5 +5,5 @@ from Configs import *
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.INFO)
 
-bot = TelegramClient('edalet', api_id=Config.APP_ID, api_hash=Config.API_HASH)
-edalet = bot.start(bot_token=Config.TOKEN)
+bot = TelegramClient('edalet', api_id=Configs.APP_ID, api_hash=Configs.API_HASH)
+edalet = bot.start(bot_token=Configs.TOKEN)
