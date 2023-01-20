f9f4b24a6038df3f9c86d12224a7ae48f40de1cb
Fakebody31 <99437747+Fakebody31@users.noreply.github.com>
2023-01-20 21:59:09 +0400

    Create __main__.py

---
+++ Plugins/__main__.py
@@ -0,0 +1,21 @@
+import glob
+from pathlib import Path
+from Plugins.utils import load_edalet
+import logging
+from Plugins import edalet
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.INFO)
+
+path = "Plugins/edalet/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as a:
+        patt = Path(a.name)
+        plugin_name = patt.stem
+        load_mahoaga(plugin_name.replace(".py", ""))
+    
+print("Qoz kimi işləyirəm Ədoşş")
+
+if __name__ == "__main__":
+    edalet.run_until_disconnected()
