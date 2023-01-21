b7634a910dbfff00c67ee92c3cbe87beefc029ce
Fakebody31 <99437747+Fakebody31@users.noreply.github.com>
2023-01-21 04:32:20 +0400

    Update __init__.py

--- Plugins/__init__.py
+++ Plugins/__init__.py
@@ -1,9 +1,6 @@
 from telethon import TelegramClient 
 import logging
-from Config import *
+from komekci.edaletconfig import edalet
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.INFO)
-
-
-edalet = TelegramClient('edalet', API_ID, API_HASH).start(bot_token=bot_token)
