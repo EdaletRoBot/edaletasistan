fea266b0312b77cb8674a8e8a6ac201d1851bb42
Fakebody31 <99437747+Fakebody31@users.noreply.github.com>
2023-01-21 04:34:19 +0400

    Update and rename komekci/edalet.py to edaletconfig.py

--- edaletconfig.py
+++ edaletconfig.py
@@ -17,3 +17,5 @@ API_ID = 28054551
 API_HASH = "64a0620c2644ceff0c1058a4ffc861ad"
 bot_token = "5883816340:AAFJpGNHNPNvogeKaLH0duNJjpchsmQ1UOg"
 
+edalet = TelegramClient('edalet', API_ID, API_HASH).start(bot_token=bot_token)
+
