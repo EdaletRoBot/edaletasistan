c32561f1d707aebe4f369a91753019fbdf5faf95
Fakebody31 <99437747+Fakebody31@users.noreply.github.com>
2023-01-21 04:36:41 +0400

    Update edaletconfig.py

--- Plugins/komekci/edaletconfig.py
+++ Plugins/komekci/edaletconfig.py
@@ -1,1 +1,20 @@
+# Bu repo aykhan_s tərəfindən 29.11.2022 tarixində yığılıb
+# Bu repodan icazəsiz hər hansı kodu sətri məlumatı kopyalıyıb
+# Öz adına çıxaran peysərdi
+# Bu yazıları silmədən işlədin
 
+# t.me/RoBotlarimTg | YouTube: RoBotlarimTg |
+# t.me/aykhan_s | insta: aykhan026 | 
+# GitHub: aykhan026
+
+
+from telethon import TelegramClient
+
+# Config məlumatları
+
+# Telegram Client (Telethon)
+API_ID = 28054551
+API_HASH = "64a0620c2644ceff0c1058a4ffc861ad"
+bot_token = "5883816340:AAFJpGNHNPNvogeKaLH0duNJjpchsmQ1UOg"
+
+edalet = TelegramClient('edalet', API_ID, API_HASH).start(bot_token=bot_token)
