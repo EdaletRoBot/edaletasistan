9e4062f537f6e900ca0a5cd23017a78199ff59d0
Fakebody31 <99437747+Fakebody31@users.noreply.github.com>
2023-01-23 12:33:53 +0400

    Update __main__.py

--- Plugins/__main__.py
+++ Plugins/__main__.py
@@ -4,6 +4,7 @@ from Plugins.utils import load_edaletbalaev
 import logging
 from Plugins import edaletbalaev
 from Plugins.komekci.edaletconfig import edalet
+from Plugins.komekci.edaletconfig import bot
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.INFO)
