42ab5cc13968143160dc961ada1fb5833cde9330
Fakebody31 <99437747+Fakebody31@users.noreply.github.com>
2023-01-23 15:21:22 +0400

    Update edaletconfig.py

--- Plugins/komekci/edaletconfig.py
+++ Plugins/komekci/edaletconfig.py
@@ -1,4 +1,5 @@
 from telethon import TelegramClient
+from pyrogram import Client
 # Config məlumatları
 
 # Telegram Client (Telethon)
@@ -7,4 +8,9 @@ API_HASH = "64a0620c2644ceff0c1058a4ffc861ad"
 bot_token = "5883816340:AAFJpGNHNPNvogeKaLH0duNJjpchsmQ1UOg"
 
 edalet = TelegramClient('edalet', API_ID, API_HASH).start(bot_token=bot_token)
+
+app = Client("edalet", bot_token=bot_token, api_hash=API_HASH, api_id=API_ID)
+
+
+
 
