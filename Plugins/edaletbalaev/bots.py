3f5b419acdc0aed90d48250970e6c066cf53e1d0
Fakebody31 <99437747+Fakebody31@users.noreply.github.com>
2023-01-24 03:51:32 +0400

    Update and rename lgbt.py to bots.py

--- Plugins/edaletbalaev/bots.py
+++ Plugins/edaletbalaev/bots.py
@@ -8,9 +8,9 @@ async def bots(event):
     if event.fwd_from:
         return
     mentions = "**Qrupdakı botlar:**\n"
-    async for user in event.edalet.iter_participants(event.chat_id, filter=ChannelParticipantsBots):
+    async for user in event.client.iter_participants(event.chat_id, filter=ChannelParticipantsBots):
         if not user.deleted:
             mentions += f"\n[{user.first_name}](tg://user?id={user.id}) `{user.id}`"
         else:
-            mentions += f"\nSilinən hesab `{user.id}`"
+            mentions += f"\nSilinən hesab`{user.id}`"
     await event.reply(mentions)
