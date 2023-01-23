332789843d12a843390896cae486bbbbba529c21
Fakebody31 <99437747+Fakebody31@users.noreply.github.com>
2023-01-23 18:37:17 +0400

    Update Config.py

--- Config.py
+++ Config.py
@@ -6,6 +6,6 @@ class Config():
     #>>> https://my.telegram.org
     API_ID = int(os.environ.get("API_ID","28054551"))
     API_HASH = os.environ.get("API_HASH","64a0620c2644ceff0c1058a4ffc861ad")
-    BOT_TOKEN = "5883816340:AAFJpGNHNPNvogeKaLH0duNJjpchsmQ1UOg"
+    BOT_TOKEN = "5628406386:AAGE4oMxDksUKHl6Urr8IeIpDHVoH8tV8PI"
     BOT_USERNAME = os.environ.get("BOT_USERNAME","Ədalət")
     BOT_NAME = os.environ.get("BOT_NAME","EdaletRoBot")
