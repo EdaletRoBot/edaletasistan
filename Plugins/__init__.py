13d001b731cc2fa124f924659197c338eae5dee7
Fakebody31 <99437747+Fakebody31@users.noreply.github.com>
2023-01-23 12:39:35 +0400

    Update __init__.py

--- Plugins/__init__.py
+++ Plugins/__init__.py
@@ -1,8 +1,6 @@
 from telethon import TelegramClient 
-from pyrogram import Client
 import logging
 from Plugins.komekci.edaletconfig import edalet
-from Plugins.komekci.edaletconfig import bot
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.INFO)
