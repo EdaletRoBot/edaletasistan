1e74ffee7221ec51b0e7cbc5d6b435f3022e68f2
EdaletRoBot <99437747+EdaletRoBot@users.noreply.github.com>
2023-01-29 21:11:15 +0400

    Update edaletconfig.py

--- Plugins/komekci/edaletconfig.py
+++ Plugins/komekci/edaletconfig.py
@@ -7,3 +7,5 @@ API_HASH = "64a0620c2644ceff0c1058a4ffc861ad"
 bot_token = "5883816340:AAELK7GZh77Q9WtBSMLVc3E6SokU6ldPLYo"
 
 edalet = TelegramClient('edalet', API_ID, API_HASH).start(bot_token=bot_token)
+
+
